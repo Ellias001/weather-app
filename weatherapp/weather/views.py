e540da20f9cc6b8e4f870f0e03f32e378b96f807
Oborotov Iliya <obronat5@gmail.com>
2020-08-13 16:12:01 +0900

    Updated view and index.html page
    Connected to an API, extracted weather data and formated it. Made more responsive web page representation.

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,4 +1,18 @@
+import requests
 from django.shortcuts import render
 
+APPID = '8486ae527aa8c0b62ca90626dd1c24f7'
+
 def index(request):
-    return render(request, 'weather/index.html')
+    city='Busan'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={APPID}'
+    
+    responce = requests.get(url).json()
+    
+    city_info = {
+        'city': city,
+        'temp': responce["main"]["temp"],
+        'icon': responce["weather"][0]["icon"]
+    }
+
+    return render(request, 'weather/index.html', {'info': city_info})
