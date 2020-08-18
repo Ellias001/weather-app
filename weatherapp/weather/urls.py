19151b9ba23ed3ba36f44f3c36c2739e16137633
Oborotov Iliya <obronat5@gmail.com>
2020-08-18 15:26:14 +0900

    Added feature to delete cities.
    Added a button, new url and view.

--- weatherapp/weather/urls.py
+++ weatherapp/weather/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.delete_city, name='delete_city'),
 ]
--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,12 +1,12 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import City
 from .forms import CityForm
 
 APPID = '8486ae527aa8c0b62ca90626dd1c24f7'
+URL = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + APPID
 
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + APPID
     cities = City.objects.all()
     all_cities = list()
 
@@ -17,12 +17,18 @@ def index(request):
     form = CityForm() # clears the form object
 
     for city in cities:
-        responce = requests.get(url.format(city.name)).json()
+        responce = requests.get(URL.format(city.name)).json()
         city_info = {
             'city': city.name,
+            'id': city.id,
             'temp': responce["main"]["temp"],
             'icon': responce["weather"][0]["icon"]
         }
         all_cities.append(city_info) 
 
     return render(request, 'weather/index.html', {'all_info': all_cities, 'form': form})
+
+def delete_city(request, pk):
+    city = get_object_or_404(City, pk=pk)
+    city.delete()
+    return redirect('/')
